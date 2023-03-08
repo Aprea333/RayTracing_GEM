5dc1c948af8192bb4d341f735bceb91a082943b9
Emanuele <emanuelericci1998@libero.it>
2023-03-08 12:52:19 +0100

    Prodotto scalare e tensoriale

--- RayTracing/Color.cs
+++ RayTracing/Color.cs
@@ -1,4 +1,45 @@
 
-class Color{
+using System.Runtime.CompilerServices;
 
+class Color
+{
+    public float r;
+    public float g;
+    public float b;
+
+    public Color(float r, float g, float b)
+    {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public void stampa()
+    {
+        Console.WriteLine(r);
+        Console.WriteLine(g);
+        Console.WriteLine(b);
+    }
+
+    public static Color operator *(Color cc, float alpha)
+    {
+        cc.r *= alpha;
+        cc.g *= alpha;
+        cc.b *= alpha;
+
+        return cc;
+    }
+
+    public static Color operator *(Color a, Color b)
+    {
+        Color prod = new Color(0f,0f, 0f);
+        prod.r = a.r * b.r;
+        prod.g = a.g * b.g;
+        prod.b = a.b * b.b;
+        
+        return prod;
+    }
+    
+    
+    
 }
--- RayTracing/Program.cs
+++ RayTracing/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-
+Color c = new Color(2f, 3f, 5f);
+Color a = new Color(3f, 6f, 7f);
+Color b = new Color(1f, 8f, 3f);
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, GEM!");
-
-
+c.stampa();
+/*
+c = c * 3;
+c.stampa();
+*/
+c = a * b;
+c.stampa();
