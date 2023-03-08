88a2e4955ed71d54074e900ec1838a2f39c483ba
micheladinatolo <michela.dinatolo@gmail.com>
2023-03-08 12:20:42 +0100

    Implementazione classe michi

--- RayTracing/Color.cs
+++ RayTracing/Color.cs
@@ -1,4 +1,19 @@
 
-class Color{
+public class Color
+{
+    float r;
+    float g;
+    float b;
+    public Color(float rc, float gc, float bc)
+    {
+        r = rc;
+        g = gc;
+        b = bc;
+    }
 
+    public void print_color()
+    {
+        Console.WriteLine("Color: (" + r + ", " + g + ", " + b + ")");
+    }
 }
+    
--- RayTracing/Program.cs
+++ RayTracing/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-
+Color c = new Color(2.0f, 3.0f, 4.0f);
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, GEM!");
+c.print_color();
+
 
 
