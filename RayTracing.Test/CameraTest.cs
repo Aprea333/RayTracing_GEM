959e8e92eee9ba2912d82e96fceb76298be2c068
Emanuele <emanuelericci1998@libero.it>
2023-04-21 11:40:29 +0200

    Add class for orthogonal projection Add test for this class

--- RayTracing.Test/CameraTest.cs
+++ RayTracing.Test/CameraTest.cs
@@ -2,5 +2,33 @@
 
 public class CameraTest
 {
+    public static camera cam = new Orthogonal_Camera(aspect_ratio : 2.0f);
     
+    public Ray ray1 = cam.fire_ray(0.0f, 0.0f); 
+    public Ray ray2 = cam.fire_ray(1.0f, 0.0f);
+    public Ray ray3 = cam.fire_ray(0.0f, 1.0f);
+    public Ray ray4 = cam.fire_ray(1.0f, 1.0f);
+
+    [Test]
+    public void test_orthogonal_camera()
+    {
+        Assert.True(Math.Abs((ray1.Dir ^ ray2.Dir).squared_norm()) < 0.00001);
+        Assert.True(Math.Abs((ray1.Dir ^ ray3.Dir).squared_norm()) < 0.00001);
+        Assert.True(Math.Abs((ray1.Dir ^ ray4.Dir).squared_norm()) < 0.00001);
+       
+        Assert.True(Point.are_close(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
+        Assert.True(Point.are_close(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
+        Assert.True(Point.are_close(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
+        Assert.True(Point.are_close(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+    }
+
+    [Test]
+    public void test_orthogonal_camera_transform()
+    {
+        Tran tr = Tran.Translation_matr(new Vec(0.0f, -1.0f, 0.0f) * 2.0f) * Tran.Rotation_z(90);
+        Orthogonal_Camera cam = new Orthogonal_Camera(tr);
+
+        Ray r = cam.fire_ray(0.5f, 0.5f);
+        Assert.True(Point.are_close(r.At(1.0f), new Point(0.0f, -2.0f, 0.0f)));
+    }
 }
--- RayTracing/Camera.cs
+++ RayTracing/Camera.cs
@@ -1,6 +1,41 @@
 ﻿namespace RayTracing;
 
-interface camera
+public interface camera
 {
-    void fire_ray(float u, float v);
+    Ray fire_ray(float u, float v);
+}
+
+/// <summary>
+/// Class for the othogonal projection
+/// </summary>
+public class Orthogonal_Camera : camera
+{
+    public float aspect_ratio;
+    public Tran transformation;
+    
+    public Orthogonal_Camera( Tran tran = null, float aspect_ratio = 1.0f)
+    {
+        this.aspect_ratio = aspect_ratio;
+        if (tran == null)
+        {
+            this.transformation = new Tran();
+        }
+        else
+        {
+            transformation = tran;
+        }
+    }
+   
+    /// <summary>
+    /// Launch of a new ray in orthogonal projection
+    /// </summary>
+    /// <param name="u"></param>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    public Ray fire_ray(float u, float v)
+    {
+        Point origin = new Point(-1, (1.0f - 2 * u) * aspect_ratio, 2 * v - 1);
+        Vec direction = new Vec(1.0f, 0, 0);
+        return Ray.Transform(this.transformation, new Ray (origin, direction, 1.0e-5f));
+    }
 }
