720267ad65bb3aa7517c06ef9d034d9dddb5b4c1
micheladinatolo <michela.dinatolo@gmail.com>
2023-04-26 12:08:36 +0200

    Add perspective camera

--- RayTracing.Test/CameraTest.cs
+++ RayTracing.Test/CameraTest.cs
@@ -2,5 +2,34 @@
 
 public class CameraTest
 {
+    private PerspectiveCamera cam = new PerspectiveCamera(1.0f, 2.0f);
+    private Ray ray1 = cam.fire_ray(0.0f, 0.0f);
+    private Ray ray2 = cam.fire_ray(1.0f, 0.0f);
+    private Ray ray3 = cam.fire_ray(0.0f, 1.0f);
+    private Ray ray4 = cam.fire_ray(1.0f, 1.0f);
+    [Test]
+    public void test_point()
+    {
+        Assert.True(Point.are_close(ray1.Origin, ray2.Origin));
+        Assert.True(Point.are_close(ray1.Origin, ray3.Origin));
+        Assert.True(Point.are_close(ray1.Origin, ray4.Origin));
+    }
     
+    [Test]
+    public void test_point2()
+    {
+        Assert.True(Ray.are_close(ray1,ray2));
+        Assert.True(Ray.are_close(ray1,ray3));
+        Assert.True(Ray.are_close(ray1,ray4));
+    }
+
+    [Test]
+    public void test_coord()
+    {
+        Assert.True(Point.are_close(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
+        Assert.True(Point.are_close(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
+        Assert.True(Point.are_close(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
+        Assert.True(Point.are_close(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+    }
+
 }
--- RayTracing/Camera.cs
+++ RayTracing/Camera.cs
@@ -3,4 +3,29 @@
 interface camera
 {
     void fire_ray(float u, float v);
+    
+
+}
+
+public class PerspectiveCamera : camera
+{
+    private float distance;
+    private float aspect_ratio;
+    private Tran T;
+    
+    public void fire_ray(float u, float v)
+    {
+        Point origin = new Point(-this.distance, 0.0f, 0.0f);
+        Vec direction = new Vec(-distance, (float)((1.0 - 2 * u) * aspect_ratio), 2 * v - 1);
+        Ray r = new Ray(origin, direction, 1.0f);
+        return r.transform(T);
+    }
+
+    public PerspectiveCamera(float d = 1.0f, float a =1.0f, Tran T = null)
+    {
+        this.distance = d;
+        this.aspect_ratio = a;
+        this.T = T;
+    }
+
 }
