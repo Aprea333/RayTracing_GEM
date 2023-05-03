64e833e47156b3199fadf68176b0a03ee3bb8376
Emanuele <emanuelericci1998@libero.it>
2023-05-03 12:09:32 +0200

    Add class World

---
+++ RayTracing/World.cs
@@ -0,0 +1,32 @@
+﻿namespace RayTracing;
+
+public class World
+{
+    public Shape[] sh;
+
+    public void add(Shape s)
+    {
+        sh.Append(s);
+    }
+
+    public HitRecord? ray_intersection(Ray r)
+    {
+        HitRecord? closest = null;
+        foreach (var shape in sh)
+        {
+            HitRecord? intersection = shape.ray_intersection(r);
+            
+            if (intersection != null)
+            {
+                continue;
+            }
+            
+            if ((closest != null) || (intersection?.t < closest?.t))
+            {
+                closest = intersection;
+            }
+        }
+
+        return closest;
+    }
+}
