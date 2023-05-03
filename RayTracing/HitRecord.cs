1572245cf17b30e124cadebd3114145262c7d370
micheladinatolo <michela.dinatolo@gmail.com>
2023-05-03 11:11:09 +0200

    Add HitRecord.cs

---
+++ RayTracing/HitRecord.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace RayTracing;
+
+public struct HitRecord
+{
+    public Point world_point;
+    public Normal normal;
+    public Vector2 surface_point;
+    public float t;
+    public Ray ray;
+}
