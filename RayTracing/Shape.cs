ea788c690b601cbeecc9e4f5658ecd596e6c6332
Emanuele <emanuelericci1998@libero.it>
2023-05-03 11:17:50 +0200

    Add abstract class Shape

---
+++ RayTracing/Shape.cs
@@ -0,0 +1,12 @@
+﻿namespace RayTracing;
+
+public abstract class Shape
+{
+    public Tran transformation;
+    public Shape(Tran tr)
+    {
+        transformation = tr;
+    }
+
+    public abstract HitRecord ray_intersection(Ray r);
+}
