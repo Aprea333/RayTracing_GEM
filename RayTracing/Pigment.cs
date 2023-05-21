7a9a7273333cb3924a5b077253f7811e5655a796
Emanuele <emanuelericci1998@libero.it>
2023-05-21 19:03:54 +0200

    remove class material from Pigment

--- RayTracing/Pigment.cs
+++ RayTracing/Pigment.cs
@@ -70,16 +70,3 @@ public class ImagePigment : Pigment
         return image.get_pixel(col, row);
     }
 }
-
-public class Material
-{
-    public Brdf brdf;
-    public Pigment emitted_radiance;
-
-    public Material(Brdf? brdf = null, Pigment? emitted_radiance = null)
-    {
-        this.brdf = brdf ?? new DiffuseBrdf();
-        this.emitted_radiance = emitted_radiance ?? new UniformPigment(Colour.black);
-    }
-
-}
