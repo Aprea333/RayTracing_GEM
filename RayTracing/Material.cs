db4b09353c5cbc4d21ba7a63b371185e42c71dd1
Emanuele <emanuelericci1998@libero.it>
2023-05-21 19:04:35 +0200

    add constructor in Material.cs

--- RayTracing/Material.cs
+++ RayTracing/Material.cs
@@ -2,6 +2,12 @@
 
 public class Material
 {
-    public Brdf brdf = new DiiffuseBrdf();
-    public Pigment emitted_radiance = new UniformPigment(BLACK);
+    public Brdf brdf = new DiffuseBrdf();
+    public Pigment emitted_radiance = new UniformPigment(Colour.black);
+    
+    public Material(Brdf? brdf = null, Pigment? emitted_radiance = null)
+    {
+        this.brdf = brdf ?? new DiffuseBrdf();
+        this.emitted_radiance = emitted_radiance ?? new UniformPigment(Colour.black);
+    }
 }   
