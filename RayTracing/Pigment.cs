8eba6354a97d6c6e3b9ca4aba841917f09a9ce81
micheladinatolo <michela.dinatolo@gmail.com>
2023-05-21 16:43:53 +0200

    Add class Material in Pigment.cs

--- RayTracing/Pigment.cs
+++ RayTracing/Pigment.cs
@@ -70,3 +70,16 @@ public class ImagePigment : Pigment
         return image.get_pixel(col, row);
     }
 }
+
+public class Material
+{
+    public Brdf brdf;
+    public Pigment emitted_radiance;
+
+    public Material(Brdf? brdf = null, Pigment? emitted_radiance = null)
+    {
+        this.brdf = brdf ?? new DiffuseBrdf();
+        this.emitted_radiance = emitted_radiance ?? new UniformPigment(Colour.black);
+    }
+
+}
