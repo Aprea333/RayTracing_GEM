22efed917f22732b2ca94d8b198f047cb80fddeb
Emanuele <emanuelericci1998@libero.it>
2023-05-24 11:31:04 +0200

    Add ScatterRay method in BRDF and DiffuseBrdf

--- RayTracing/BRDF.cs
+++ RayTracing/BRDF.cs
@@ -11,6 +11,8 @@ public abstract class Brdf
 
     public abstract Colour Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv);
 
+    public abstract Ray ScatterRay(PCG pcg, Vec incoming_dir, Point interaction_point, Normal normal, int depth);
+
 }
 
 public class DiffuseBrdf : Brdf
@@ -20,6 +22,20 @@ public class DiffuseBrdf : Brdf
     public override Colour Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv)
     {
         return Pig.get_colour(uv) * (float)(1.0f / Math.PI);
+    }
+
+    public override Ray ScatterRay(PCG pcg, Vec incoming_dir, Point interaction_point, Normal normal, int depth)
+    {
+        (Vec e1, Vec e2, Vec e3) = create_onb_from_z(normal); // L'ho fatto immaginando che "create_onb_from_z" ritorni una tupla
+        float cos_theta_sq = pcg.random_float();
+        float cos_theta = (float)Math.Sqrt(cos_theta_sq);
+        float sin_theta = (float)Math.Sqrt(1.0 - cos_theta_sq);
+        float phi = (float)(2.0 * Math.PI * pcg.random_float());
+        return new Ray(interaction_point,
+            e1 * (float)Math.Cos(phi) * cos_theta + e2 *(float) Math.Sin(phi) * cos_theta + e3 * sin_theta,
+            1.0e-3f,
+            Single.PositiveInfinity,
+            depth);
     }
 }
 
