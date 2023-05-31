54c8130034bcf8ef3bc8f2d86908387d7a030418
Giacomo_Aprea <geki.aprea99@gmail.com>
2023-05-31 11:00:10 +0200

    Add SourceLocation

--- RayTracing/Scene.cs
+++ RayTracing/Scene.cs
@@ -1,6 +1,15 @@
 ﻿namespace RayTracing;
 
-public class Scene
+public struct SourceLocation
 {
-    
+  public string file_name = "";
+  public int line_num = 0;
+  public int col_num = 0;
+  public SourceLocation(string fileName, int lineNum, int colNum)
+  {
+    file_name = fileName;
+    line_num = lineNum;
+    col_num = colNum;
+  }
 }
+
