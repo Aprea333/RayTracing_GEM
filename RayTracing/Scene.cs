3105b6844aa594dfea590bd34f4beda32e9c192a
Giacomo_Aprea <geki.aprea99@gmail.com>
2023-05-31 12:39:19 +0200

    Add KeyWordToken

--- RayTracing/Scene.cs
+++ RayTracing/Scene.cs
@@ -1,4 +1,7 @@
-﻿namespace RayTracing;
+﻿using System.Net.Sockets;
+using RayTracing;
+
+namespace RayTracing;
 
 public class GrammarError: Exception
 {
@@ -23,5 +26,75 @@ public abstract class Token
 {
 
     public SourceLocation Location;
+
     
 }
+
+
+public enum EnumKeyword 
+{
+    New,
+    Box,
+    Brdf,
+    Camera,
+    Colour,
+    Material,
+    Diffuse,
+    Uniform,
+    Checkered,
+    Image,
+    Translation,
+    Pigment,
+    World,
+    Orthogonal,
+    Perspective,
+    RotationX,
+    RotationY,
+    RotationZ,
+    Float,
+    Scaling
+}
+
+public class KeywordToken : Token
+{
+    public EnumKeyword keyword;
+
+    public static IDictionary<string, EnumKeyword> Dict = new Dictionary<string, EnumKeyword>
+    {
+        { "new", EnumKeyword.New },
+        { "Box", EnumKeyword.Box },
+        { "Brdf", EnumKeyword.Brdf },
+        { "Camera", EnumKeyword.Camera },
+        { "Colour", EnumKeyword.Colour },
+        { "Material", EnumKeyword.Material },
+        { "Diffuse", EnumKeyword.Diffuse },
+        { "Uniform", EnumKeyword.Uniform },
+        { "Checkered", EnumKeyword.Checkered },
+        { "Image", EnumKeyword.Image },
+        { "Translation", EnumKeyword.Translation },
+        { "Pigment", EnumKeyword.Pigment },
+        { "World", EnumKeyword.World },
+        { "Orthogonal", EnumKeyword.Orthogonal },
+        { "Perspective", EnumKeyword.Perspective },
+        { "rotation_x", EnumKeyword.RotationX },
+        { "rotation_y", EnumKeyword.RotationY },
+        { "rotation_z", EnumKeyword.RotationZ },
+        { "float", EnumKeyword.Float },
+        { "scale", EnumKeyword.Scaling }
+    };
+
+    public KeywordToken(SourceLocation Location,EnumKeyword keyword)
+    {
+        this.Location = Location;
+        this.keyword = keyword;
+    }
+    
+    public string Write()
+    {
+        return keyword.ToString();
+    }
+}
+
+
+
+
